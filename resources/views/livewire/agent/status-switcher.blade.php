02825cd3dabe7ce588f451b65313fcb4f1ce9533
rgabuat <73892616+rgabuat@users.noreply.github.com>
2026-02-12 00:50:18 +0800

    button-3

--- resources/views/livewire/agent/status-switcher.blade.php
+++ resources/views/livewire/agent/status-switcher.blade.php
@@ -2,7 +2,7 @@
 
     {{-- STATUS DROPDOWN --}}
     <div class="relative" x-data="{ open: false }">
-        <button
+        <button type="button"
             @click="open = !open"
             class="flex items-center gap-2 rounded-lg
                    bg-zinc-800 px-3 py-2 text-sm
