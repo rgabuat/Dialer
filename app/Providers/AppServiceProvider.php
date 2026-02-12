936b534127ec9095446d87147520e422249f6fe9
rgabuat <73892616+rgabuat@users.noreply.github.com>
2026-02-13 00:50:27 +0800

    test3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,7 +7,6 @@ use Livewire\Livewire;
 use App\Models\UsersMeta;
 use App\Observers\UserObserver;
 use App\Observers\UserMetaObserver;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
@@ -40,8 +39,6 @@ class AppServiceProvider extends ServiceProvider
             Livewire::setUpdateRoute(function ($handle) use ($basePath) {
                 return Route::post($basePath . '/livewire/update', $handle);
             });
-
-            URL::forceScheme('https');
 
         });
     }
